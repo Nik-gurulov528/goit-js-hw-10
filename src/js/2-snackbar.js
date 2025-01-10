36bc671c462f17598aff7cc0c931ef2c2420fbfc
Neon <neonmed571@gmail.com>
2025-01-10 09:50:45 +0200

    finish second task

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,60 @@
+'use strict';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const timeDelay = document.querySelector('input');
+const promiseForm = document.querySelector('form');
+const successfulOpt = document.querySelectorAll('input[name = "state"]')[0];
+const unsuccessfulOpt = document.querySelectorAll('input[name = "state"]')[1];
+let selectedOpt = null;
+
+let options = {
+  titleColor: '#ffffff',
+  messageColor: '#ffffff',
+  close: false,
+  pauseOnHover: false,
+  position: 'center',
+};
+
+let delayMs = timeDelay.value;
+timeDelay.addEventListener('input', () => {
+  delayMs = timeDelay.value;
+});
+
+successfulOpt.addEventListener('click', () => {
+  selectedOpt = successfulOpt;
+});
+unsuccessfulOpt.addEventListener('click', () => {
+  selectedOpt = unsuccessfulOpt;
+});
+
+promiseForm.addEventListener('submit', event => {
+  event.preventDefault();
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (selectedOpt === successfulOpt) {
+        resolve(`Fulfilled promise in ${delayMs}ms`);
+      } else {
+        reject(`Rejected promise in ${delayMs}ms`);
+      }
+
+      promise
+        .then(strText => {
+          options.title = 'Success!';
+          options.message = strText;
+          options.backgroundColor = '#02d93b';
+          options.progressBarColor = '#05961a';
+          iziToast.show(options);
+        })
+        .catch(errorText => {
+          options.title = 'Fail!';
+          options.message = errorText;
+          options.backgroundColor = '#d61c0f';
+          options.progressBarColor = '#611101';
+          iziToast.show(options);
+        });
+    }, delayMs);
+  });
+  promiseForm.reset();
+});
